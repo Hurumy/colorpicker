13c60264843c2ee6429f5a9d0a3bf2a94e55a25e
Hurumy <sarrendish@gmail.com>
2023-10-05 17:20:01 +0900

    なんか急に全部理解してキー入力を実装できた。なにこれ？

--- app/Main.hs
+++ app/Main.hs
@@ -20,10 +20,10 @@ window = InWindow "ColorPicker" (wWidth, wHeight) (100, 100)
 main :: IO ()
 main = do
 	info <- initInfo
-	display window white $ testWindow info
+	playIO window white 10 info testWindow eventHandler stepInfo
 
-testWindow :: Info -> Picture
-testWindow i@Info{..} = pictures[ translate 100 100 $ color (makeColorI _red _green _blue 255) $ circleSolid 50]
+testWindow :: Info -> IO Picture
+testWindow i@Info{..} = pure $ pictures[ translate 100 100 $ color (makeColorI _red _green _blue 255) $ circleSolid 50]
 
 data Info = Info
 	{ _key :: KeyInput
@@ -38,16 +38,14 @@ initInfo = pure $ Info {_key = INone, _red = 200, _green = 200, _blue = 100}
 
 data KeyInput = IUpR | IDwR | IUpG | IDwG | IUpB | IDwB | INone
 
-{--
-changeColor :: KeyInput -> PickedColor -> PickedColor
-changeColor IUpR p@PickedColor{..} = p{ _red = _red + 1 }
-changeColor IDwR p@PickedColor{..} = p{ _red = _red - 1 }
-changeColor IUpG p@PickedColor{..} = p{ _green = _green + 1 }
-changeColor IDwG p@PickedColor{..} = p{ _green = _green - 1 }
-changeColor IUpB p@PickedColor{..} = p{ _blue = _blue + 1 }
-changeColor IDwB p@PickedColor{..} = p{ _blue = _blue - 1 }
---}
-
+changeColor :: KeyInput -> Info -> IO Info
+changeColor IUpR p@Info{..} = pure $ p{ _red = _red + 1 }
+changeColor IDwR p@Info{..} = pure $ p{ _red = _red - 1 }
+changeColor IUpG p@Info{..} = pure $ p{ _green = _green + 1 }
+changeColor IDwG p@Info{..} = pure $ p{ _green = _green - 1 }
+changeColor IUpB p@Info{..} = pure $ p{ _blue = _blue + 1 }
+changeColor IDwB p@Info{..} = pure $ p{ _blue = _blue - 1 }
+changeColor _ p = pure $ p
 
 {--
 pickColor :: PickedColor -> Color
@@ -62,7 +60,6 @@ updateWindow i@Info{..} =
 	--, translate (-wWidth/2+10) (-wHeight/2+10) . scale 0.2 0.2 $ text ("Red: " ++ show _red ++ " Green: " ++ show _green ++ " Blue: " ++ show _blue)
 	]
 
-{--
 eventHandler :: Event -> Info -> IO Info
 eventHandler e i = case e of
 	EventKey (Char 'f')		Down _ _ -> pure $ i { _key = IUpR }
@@ -73,6 +70,11 @@ eventHandler e i = case e of
 	EventKey (Char 'n')		Down _ _ -> pure $ i { _key = IDwB }
 	_ -> pure i
 
+stepInfo :: Float -> Info -> IO Info
+stepInfo f i@Info{..} = changeColor _key i
+
+
+{--
 stepInfo :: Info -> IO Info
 stepInfo i@Info{..} = do
 	let newcolor = changeColor _key _color
